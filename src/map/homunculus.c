5c4715cd4ff927d436aa55dc18931d73271b5595
Mark Steve Samson <hello@marksteve.com>
2020-04-16 07:58:24 +0800

    Fix missing semi-colons

--- src/map/homunculus.c
+++ src/map/homunculus.c
@@ -594,7 +594,7 @@ static unsigned int homunculus_add_intimacy(struct homun_data *hd, unsigned int 
 	if (battle_config.homunculus_friendly_rate != 100)
 		value = (value * battle_config.homunculus_friendly_rate) / 100;
 
-	value *= 5
+	value *= 5;
 
 	if (hd->homunculus.intimacy + value <= 100000)
 		hd->homunculus.intimacy += value;
