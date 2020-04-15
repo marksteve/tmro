1b92af8face672f02d1a5bec9045bc22193eadf9
Mark Steve Samson <hello@marksteve.com>
2020-04-16 07:50:53 +0800

    Rebuild server on deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 # Deploy
 curl https://cronitor.link/WvZpY3/run -m 10 || true
 git pull --rebase
+make clean
+./configure --enable-packetver=20181226
+make
 ./athena-start restart
 curl https://cronitor.link/WvZpY3/complete -m 10 || true
 # Update MOTD
