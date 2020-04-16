eddf2148363afd4b553d95a854e1c619edbf670e
Mark Steve Samson <hello@marksteve.com>
2020-04-16 23:09:08 +0800

    Rollback pet and homunclus changes in favor of config updates

--- src/map/homunculus.c
+++ src/map/homunculus.c
@@ -594,8 +594,6 @@ static unsigned int homunculus_add_intimacy(struct homun_data *hd, unsigned int 
 	if (battle_config.homunculus_friendly_rate != 100)
 		value = (value * battle_config.homunculus_friendly_rate) / 100;
 
-	value *= 2.5;
-
 	if (hd->homunculus.intimacy + value <= 100000)
 		hd->homunculus.intimacy += value;
 	else
